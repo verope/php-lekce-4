ad67ab4654a7120d81eb8914e9ca6806b392f181
Luděk Veselý <ludek.vesely@email.com>
2018-10-09 01:43:03 +0200

    add second task

---
+++ ukol-2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Úkol 2</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br>
+<div class="container">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Číslo objednávky</th>
+                <th>Jméno zákazníka</th>
+                <th>Datum vytvoření objednávky</th>
+                <th>Celková cena</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>123</td>
+            <td>Josef</td>
+            <td>1.1.2000</td>
+            <td>0 Kč</td>
+        </tr>
+    </table>
+</div>
+</body>
+</html>
+
